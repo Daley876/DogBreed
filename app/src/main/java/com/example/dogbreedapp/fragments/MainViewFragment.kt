8d61a0b9c45de46e172af5fa954b8b87bf77f126
sheckardodaley <sheckardodaley@gmail.com>
2023-01-30 17:11:24 -0500

    additional ui update for error handling

--- app/src/main/java/com/example/dogbreedapp/fragments/MainViewFragment.kt
+++ app/src/main/java/com/example/dogbreedapp/fragments/MainViewFragment.kt
@@ -7,13 +7,18 @@ import android.view.ViewGroup
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.RectangleShape
@@ -72,28 +77,53 @@ class MainViewFragment : Fragment() {
     @Composable
     fun InputRow() {
         Column {
-            Row(modifier = Modifier.padding(top = 15.dp, start = 12.dp)) {
+            Row(modifier = Modifier.padding(top = 55.dp, start = 12.dp)) {
                 val input = remember { mutableStateOf("") }
-                TextField(
-                    value = input.value,
-                    textStyle = TextStyle.Default.copy(fontSize = 28.sp),
-                    singleLine = true,
-                    onValueChange = { input.value = it },
-                    label = { Text(text = "Dog Breed", fontSize = 16.sp) },
-                    modifier = Modifier
-                        .width(240.dp)
-                        .wrapContentHeight()
-                        .border(border = BorderStroke(2.dp, colorResource(id = R.color.black)))
-                )
-
+                val isValid = remember { mutableStateOf(true) }
+                Column {
+                    TextField(
+                        value = input.value,
+                        trailingIcon = {
+                            if (isValid.value) {
+                                Icon(Icons.Default.Clear, contentDescription = "clear text",
+                                    modifier = Modifier.clickable { input.value = "" })
+                            } else {
+                                Icon(
+                                    Icons.Filled.Info, contentDescription = "error icon",
+                                    tint = colorResource(id = R.color.red)
+                                )
+                            }
+                        },
+                        textStyle = TextStyle.Default.copy(fontSize = 28.sp),
+                        singleLine = true,
+                        onValueChange = { input.value = it },
+                        label = { Text(text = "Dog Breed", fontSize = 16.sp) },
+                        modifier = Modifier
+                            .width(240.dp)
+                            .wrapContentHeight()
+                            .border(border = BorderStroke(2.dp, colorResource(id = R.color.black)))
+                    )
+                    if (!isValid.value) {
+                        Text(
+                            text = "Breed cannot be blank!",
+                            color = MaterialTheme.colors.error,
+                            style = MaterialTheme.typography.caption,
+                            modifier = Modifier.padding(start = 12.dp, top = 3.dp)
+                        )
+                    }
+                }
                 Button(
                     onClick = {
                         val searchedTerm = input.value.lowercase(Locale.getDefault()).trim()
-                        viewModel.getDogsByBreed(searchedTerm)
+                        if (searchedTerm.isEmpty()) isValid.value = false
+                        else {
+                            isValid.value = true
+                            viewModel.getDogsByBreed(searchedTerm)
+                        }
                     },
                     modifier = Modifier
                         .wrapContentSize()
-                        .padding(start = 40.dp)
+                        .padding(start = 40.dp, top = 10.dp)
                 ) {
                     Text(
                         text = "Fetch!",
@@ -126,24 +156,32 @@ class MainViewFragment : Fragment() {
         val dogsList = viewModel.dogBreedStateData.value //value changes after each call
 
         if (dogsList.message.isNotEmpty()) {
-            LazyColumn(modifier = Modifier
-                .padding(top = 15.dp, start = 11.dp))
+            LazyColumn(
+                modifier = Modifier
+                    .padding(top = 30.dp, start = 11.dp, end = 11.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
             {
-                items(dogsList.message) { dogImgUrl ->
+                itemsIndexed(dogsList.message) { index, dogImgUrl ->
                     Card(
                         modifier = Modifier
                             .padding(12.dp)
-                            .border(BorderStroke(1.dp,colorResource(id = R.color.black)))
+                            .border(BorderStroke(1.dp, colorResource(id = R.color.black)))
                             .clip(RectangleShape)
                             .wrapContentSize()
                     ) {
-                        Row {
+                        Column {
                             AsyncImage(
                                 model = dogImgUrl,
                                 contentScale = ContentScale.Fit,
                                 contentDescription = null
                             )
                         }
+                    }
+                    if (index < dogsList.message.lastIndex) {
+                        Divider(color = colorResource(id = R.color.black), thickness = 1.dp)
                     }
                 }
             }
@@ -155,7 +193,7 @@ class MainViewFragment : Fragment() {
 
     @Composable
     fun Banner() {
-        Column(modifier = Modifier.padding(top = 20.dp)) {
+        Column {
             Text(
                 text = "Dog Breed Finder",
                 fontSize = 32.sp,
